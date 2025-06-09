c4982efccd5a12a89cee6be0752dc9bdce8a1e5d
Ouadie <elamraniouadie@gmail.com>
2025-06-09 20:43:26 +0200

    First Challenge: Using Inbuild  Methods

--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
 // Given a string, return a new string with the reversed order of characters
 
-// First method
+// First Method
 
-function reverse(str){
+function reverse_1(str){
     let reversed = "";
     for (let i = 0; i < str.length; i++){
         reversed = str[i] + reversed;
@@ -10,16 +10,28 @@ function reverse(str){
     return reversed;
 };
 
-console.log(reverse('valahala'));
+console.log(reverse_1('valahala'));
 
-// Second method
+// Second Method
 
 function reverse_2(str){
     let reversed = "";
-    for( let i = str.length - 1; i <= 0; i--){
+    for( let i = str.length - 1; i >= 0; i--){
         reversed = reversed + str[i];
     }
     return reversed;
 }
 
-console.log(reverse('Hello How Are You Today'));
+console.log(reverse_2('Hello How Are You Today'));
+
+// Third Method : using inbuild JS methods
+
+function reverse_3(str){
+    let reversed = str.split('').reverse().join('');
+    
+    return reversed;
+}
+
+console.log(reverse_3('Using Inbuild Methods'));
+
+
