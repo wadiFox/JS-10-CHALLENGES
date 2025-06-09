5754abd82f1791f55800613f29e8a257517318bb
Ouadie <elamraniouadie@gmail.com>
2025-06-09 21:30:05 +0200

    Second Challenge: Return true if palindrome

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+// Given a string, return true if the string is a palindrome
+
+function isPalindrome(str){
+    // Convert the string to lowercase to ignore case sensitivity
+    str = str.toLowerCase();
+    const reversed = str.split('').reverse().join('');
+    // Compare the original string with the reversed string
+    return str === reversed;
+}
+
+console.log(isPalindrome('kayak')); // True
+console.log(isPalindrome('kAYak')); // True
+console.log(isPalindrome('kayaki')); // False
