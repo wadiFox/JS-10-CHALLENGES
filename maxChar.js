3776355b377c378067fc84507a75781dba9cc1af
Ouadie <elamraniouadie@gmail.com>
2025-06-10 16:49:48 +0200

    Third Challeng : Max Char, Replacing if statement with one line

--- maxChar.js
+++ maxChar.js
@@ -3,21 +3,28 @@
 function maxChar(str){
     let charCount = {};
     for (let char of str) {
+         /* converting the if statement in one line code
         if (char in charCount) {
             charCount[char]++;
             } else {
                 charCount[char] = 1;
             }
+         */   
+        
+        // Replacing if statement with one line
+             charCount[char] = charCount[char] + 1 || 1 ;
         }
 console.log(charCount);                
 let maxCount = 0;
 let maxChar = '';
     for (let char in charCount) {
+       
         if (charCount[char] > maxCount) {
             maxCount = charCount[char];
             maxChar = char;
             }
         }
+
 return `${maxChar} : is used => ${maxCount} times`;
 }
 
