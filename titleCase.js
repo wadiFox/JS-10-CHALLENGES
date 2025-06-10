2fa4713d74dbc690a818a86224d18d00edfc6aad
Ouadie <elamraniouadie@gmail.com>
2025-06-10 18:45:39 +0200

    Fifth Challenge : capitalize the fisrt letter, first method

--- titleCase.js
+++ titleCase.js
@@ -1,2 +1,15 @@
 // write a function that accepts a string, and should capitalize the fisrt letter  
 // of each word in the string the return the capitalized string
+
+// First Method
+
+function capital(str){
+const result = [];
+const words = str.split(' ');
+for(let word of words){
+result.push(word[0].toUpperCase()+ word.slice(1));
+};
+return result.join(' ');;
+}
+
+console.log(capital("This is ouadie trying some coding challenges"));
