5837aeee5c25395e9efd3e7077da6dbc7dbe5d83
Ouadie <elamraniouadie@gmail.com>
2025-06-11 17:51:45 +0200

    Sixth Challenge : Replace the regex \s by \W

--- anagrams.js
+++ anagrams.js
@@ -2,12 +2,16 @@
 
 function areAnagrams(str1, str2) {
     // Remove spaces and convert to lowercase
-    str1 = str1.replace(/\s/g, "").toLowerCase();
-    str2 = str2.replace(/\s/g, "").toLowerCase();
+    // str1 = str1.replace(/\s/g, "").toLowerCase();
+    // str2 = str2.replace(/\s/g, "").toLowerCase();
+
+    // We use \W is better to remove all non alphanumeric characters
+    str1 = str1.replace(/\W/g, "").toLowerCase();
+    str2 = str2.replace(/\W/g, "").toLowerCase();
     
       // If lengths are not equal, they can't be anagrams
     if (str1.length !== str2.length) return false;
-
+    
     // Sort the characters and compare
     const sortedStr1 = str1.split("").sort().join("");
     const sortedStr2 = str2.split("").sort().join("");
@@ -15,5 +19,5 @@ function areAnagrams(str1, str2) {
     return sortedStr1 === sortedStr2;
 }
 
-console.log(areAnagrams("ahol?? tUtti?", "?ttiut? OLha?"));
+console.log(areAnagrams("ahol?? tUtti3332?", "?ttiut? 33OLha?23"));
 console.log(areAnagrams("hol??? tUtti", "ttiut OLh!!"));
